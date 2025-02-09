806cce487e4621e86297d7707a52a1382e54b5fe
vamsimunagala <munagalavamsi37@gmail.com>
2025-02-09 13:38:08 +0530

    routers fixed

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,20 +1,33 @@
-const express = require('express');
-const Event = require('../models/event');
-const authMiddleware = require('../middlewares/authMiddleware');
+const express = require("express");
+const Event = require("../models/event");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post('/create', authMiddleware, async (req, res) => {
-    const { title, description, date } = req.body;
-    const event = new Event({ title, description, date, createdBy: req.user.id });
+router.post("/create", authMiddleware, async (req, res) => {
+  const { title, description, date } = req.body;
+  const event = new Event({ title, description, date, createdBy: req.user.id });
 
-    await event.save();
-    res.json(event);
+  await event.save();
+  res.json(event);
 });
 
-router.get('/', async (req, res) => {
-    const events = await Event.find().populate('createdBy', 'name');
-    res.json(events);
+router.get("/", async (req, res) => {
+  const events = await Event.find().populate("createdBy", "name");
+  res.json(events);
+});
+
+router.get("/:id", async (req, res) => {
+  const event = await Event.findById(req.params.id).populate(
+    "createdBy",
+    "name"
+  );
+  res.json(event);
+});
+
+router.delete("/:id", authMiddleware, async (req, res) => {
+  await Event.findByIdAndDelete(req.params.id);
+  res.json({ message: "Event deleted" });
 });
 
 module.exports = router;
